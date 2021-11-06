c226c326e12a4a36b8d7222fcf4732e851562e55
riyarana10 <19323@iiitu.ac.in>
2021-11-06 00:04:21 -1200

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,16 +10,14 @@ weatherForm.addEventListener("submit", (e) => {
   const location = search.value;
   text1.textContent = "Loading...";
   text2.textContent = "";
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          text1.textContent = data.error;
-        } else {
-          text1.textContent = data.location;
-          text2.textContent = data.forecast;
-        }
-      });
-    }
-  );
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        text1.textContent = data.error;
+      } else {
+        text1.textContent = data.location;
+        text2.textContent = data.forecast;
+      }
+    });
+  });
 });
